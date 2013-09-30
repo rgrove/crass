6352489e0e4c47ad7942e12b8e972f0121f497d7
Ryan Grove <ryan@wonko.com>
2013-09-30 15:10:21 -0700

    Add serialization tests.

---
+++ test/test_serialization.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+require_relative 'support/common'
+
+describe 'Serialization' do
+  Dir[File.join(File.dirname(__FILE__), 'support/serialization/*.css')].each do |filepath|
+    it "should parse and serialize #{filepath}" do
+      input  = File.read(filepath)
+
+      tree = Crass.parse(input,
+        :preserve_comments => true,
+        :preserve_hacks => true)
+
+      output = CP.stringify(tree)
+
+      assert_equal input, output
+    end
+  end
+end
